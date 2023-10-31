e24a5eb4cab0fa09729d0494418cdefe0bb8d643
mayconbernardes <mayconwin@hotmail.com>
2023-10-31 12:25:07 +0100

    modif

--- landing.js
+++ landing.js
@@ -56,24 +56,37 @@ getProjects()
 
 
 
-        
-        document.addEventListener('DOMContentLoaded', function() {
-    var scrollToTopButton = document.getElementById('scrollToTop');
-    
-   
+     //Get the button
+let mybutton = document.getElementById("scrollToTop");
 
-    scrollToTopButton.addEventListener('click', function() {
-      window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-      });
-    });
-  });
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
 
 
 
 
 
+  scrollToTop
 
 
   
